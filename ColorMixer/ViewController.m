dd1d054908181ee8da022859c0911e6b932ecda5
Rohini Carlson <it-hogskolan@it-hogskolans-MacBook-Pro-96.local>
2015-01-22 16:10:37 +0100

    first commit

--- ColorMixer/ViewController.m
+++ ColorMixer/ViewController.m
@@ -10,12 +10,37 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *colorDisplay;
+@property (weak, nonatomic) IBOutlet UISlider *redSlider;
+@property (weak, nonatomic) IBOutlet UISlider *blueSlider;
+@property (weak, nonatomic) IBOutlet UISlider *greenSlider;
+@property (weak, nonatomic) IBOutlet UILabel *redLabel;
+@property (weak, nonatomic) IBOutlet UILabel *blueLabel;
+@property (weak, nonatomic) IBOutlet UILabel *greenLabel;
+
 @end
 
 @implementation ViewController
 
+- (UIColor*) currentColor {
+    return [UIColor colorWithRed:self.redSlider.value
+                          green:self.blueSlider.value
+                           blue:self.greenSlider.value
+                           alpha:1.0f];
+}
+
+- (IBAction)redSliderChanged:(id)sender {
+    self.colorDisplay.backgroundColor = [self currentColor];
+    self.redLabel.text = [NSString stringWithFormat:@"%.2f", self.redSlider.value];
+    self.blueLabel.text = [NSString stringWithFormat:@"%.2f", self.blueSlider.value];
+    self.greenLabel.text = [NSString stringWithFormat:@"%.2f", self.greenSlider.value];
+}
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self redSliderChanged:nil];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
